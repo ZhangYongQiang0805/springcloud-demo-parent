34caa98d97a602b331a94cc958f54d6c267c1c27
yongqiang.zhang <xx@163.com>
2019-03-09 11:23:00 +0800

    add activemq

--- springcloud-member/src/main/java/com/demo/service/impl/MemberServiceImpl.java
+++ springcloud-member/src/main/java/com/demo/service/impl/MemberServiceImpl.java
@@ -1,12 +1,16 @@
 package com.demo.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.demo.base.BaseController;
 import com.demo.base.BaseRedisService;
 import com.demo.base.ResponseBase;
 import com.demo.dao.UserDao;
 import com.demo.model.UserEntity;
+import com.demo.mq.RegisterMailboxProducer;
 import com.demo.service.MemberService;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +22,12 @@ public class MemberServiceImpl extends BaseController implements MemberService {
 
     @Autowired
     private BaseRedisService baseRedisService;
-
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private RegisterMailboxProducer registerMailboxProducer;
+    @Value("${messages.queue}")
+    private String MESSAGES_QUEUE;
 
     @Override
     public Map<String, Object> testRest() {
@@ -67,6 +74,26 @@ public class MemberServiceImpl extends BaseController implements MemberService {
         if (i <= 0) {
             return setResultError("注册失败!");
         }
+        // 采用MQ异步发送邮件
+        String email = user.getEmail();
+        String messAageJson = message(email);
+        sendMsg(messAageJson);
         return setResultSuccess("注册成功!");
+    }
+
+    private String message(String mail) {
+        JSONObject root = new JSONObject();
+        JSONObject header = new JSONObject();
+        header.put("interfaceType", "sms_mail");
+        JSONObject content = new JSONObject();
+        content.put("mail", mail);
+        root.put("header", header);
+        root.put("content", content);
+        return root.toJSONString();
+    }
+
+    private void sendMsg(String json) {
+        ActiveMQQueue activeMQQueue = new ActiveMQQueue(MESSAGES_QUEUE);
+        registerMailboxProducer.sendMsg(activeMQQueue, json);
     }
 }
