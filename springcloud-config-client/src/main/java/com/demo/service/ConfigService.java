659240316ebb5cf84ef952c4d0efb1ae3133e1ec
yongqiang.zhang <xx@163.com>
2019-03-13 23:33:42 +0800

    config-client test ok

--- springcloud-config-client/src/main/java/com/demo/service/ConfigService.java
+++ springcloud-config-client/src/main/java/com/demo/service/ConfigService.java
@@ -1,5 +1,6 @@
 package com.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope//开启刷新
 public class ConfigService {
 
-//    @Value("${info.profile}")
-    private String value;
+    @Value("${info}")
+    private String values;
 
     @RequestMapping("/")
     public String getValue() {
-        return "从Github仓库中获取的信息：【" +value + "】";
+        return "从Github仓库中获取的信息：【" + values + "】";
     }
 }
